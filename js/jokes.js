6c41475ce54c644bb58b12095097274fbfc19254
patshia <111842731+patshia@users.noreply.github.com>
2023-03-20 21:40:28 +0000

    Mon, Mar 20, 2023, 2:40 PM -07:00

---
+++ js/jokes.js
@@ -0,0 +1,8 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+function generateJoke(){
+    
+}
