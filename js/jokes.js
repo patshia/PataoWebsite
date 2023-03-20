4d6c7e865887d28b30798898298d57bfef369f8b
patshia <111842731+patshia@users.noreply.github.com>
2023-03-20 21:44:50 +0000

    Mon, Mar 20, 2023, 2:44 PM -07:00

--- js/jokes.js
+++ js/jokes.js
@@ -3,6 +3,11 @@ const jokeBtn = document.getElementById('jokeBtn')
 
 jokeBtn.addEventListener('click', generateJoke)
 
-function generateJoke(){
-    
+async function generateJoke(){
+    const response = await fetch('https://icanhazdadjoke.com', {
+        headers: {Accept: 'application/json'}
+    })
+
+    const data = await response.json()
+    jokeEl.innerHTML = data.joke
 }
