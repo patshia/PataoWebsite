b6e6bb8a7a836aa1c7655d5cede5e01513b9243b
patshia <111842731+patshia@users.noreply.github.com>
2023-03-21 21:51:13 +0000

    Tue, Mar 21, 2023, 2:51 PM -07:00

--- js/counter.js
+++ js/counter.js
@@ -4,8 +4,14 @@ counters.forEach(counter => {
     counter.innerText = '0'
 
     const updateCounter = () => {
-        const target = counter.getAttribute('data-target')
-        console.log(target)
+        const target = +counter.getAttribute('data-target')
+        const c = +counter.innerText
+        const increment = 1
+
+        if (c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`
+        }
+
     }
 
     updateCounter()
