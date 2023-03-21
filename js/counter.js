07994763cd5d91f9a6ffacd084c62c28738ac784
patshia <111842731+patshia@users.noreply.github.com>
2023-03-21 21:45:51 +0000

    Tue, Mar 21, 2023, 2:45 PM -07:00

--- js/counter.js
+++ js/counter.js
@@ -1,2 +1,12 @@
 const counters = document.querySelectorAll('.counter')
 
+counters.forEach(counter => {
+    counter.innerText = '0'
+
+    const updateCounter = () => {
+        const target = counter.getAttribute('data-target')
+        console.log(target)
+    }
+
+    updateCounter()
+})
