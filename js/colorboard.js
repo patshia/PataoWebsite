dfcfc1b9d6e16bc83b261dc9d93dd5166a266694
patshia <111842731+patshia@users.noreply.github.com>
2023-03-23 21:13:18 +0000

    Thu, Mar 23, 2023, 2:13 PM -07:00

--- js/colorboard.js
+++ js/colorboard.js
@@ -7,9 +7,14 @@ for (let i = 0; i < SQUARES; i++) {
     square.classList.add('square')
     container.appendChild(square)
     square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseout', () => removeColor(square))
 }
 
 function setColor(element) {
     const color = colors[Math.floor(Math.random() * 5)]
     element.style.background = color
+}
+
+function removeColor(element) {
+    element.style.background = '#1f1f1f'
 }
