aecfbb4f0757bf760b9488f7605bb2f4d127689c
patshia <111842731+patshia@users.noreply.github.com>
2023-03-23 21:35:02 +0000

    Thu, Mar 23, 2023, 2:35 PM -07:00

--- js/colorboard.js
+++ js/colorboard.js
@@ -1,5 +1,5 @@
 const container = document.getElementById('container')
-const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
+const colors = ['#bbee9d', '#57a87e', '#2f7633', '#285438', '']
 const SQUARES = 500
 
 for (let i = 0; i < SQUARES; i++) {
