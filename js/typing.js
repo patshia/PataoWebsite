8a5a7e89aa24611d20e0b70c7e724eb1024ef40c
patshia <111842731+patshia@users.noreply.github.com>
2023-03-24 22:03:17 +0000

    Fri, Mar 24, 2023, 3:03 PM -07:00

--- js/typing.js
+++ js/typing.js
@@ -3,13 +3,15 @@ const speedEl = document.getElementById('speed')
 const text = "We LOVE The Untamed"
 let idx = 1;
 let speed = 100 / speedEl.value
+speedEl.addEventListener('input', (element) => {
+    speed = 300 / element.target.value
+})
 
 writeText()
 
 function writeText(){
     textEl.innerText = text.slice(0,idx)
     idx++
-
     if (idx > text.length){
         idx = 1
     }
