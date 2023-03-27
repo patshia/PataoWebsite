f08e66b06b3c124cefd5b441dd674c64a4387859
patshia <111842731+patshia@users.noreply.github.com>
2023-03-27 21:24:06 +0000

    Mon, Mar 27, 2023, 2:24 PM -07:00

--- js/typing.js
+++ js/typing.js
@@ -2,7 +2,7 @@ const textEl = document.getElementById('text')
 const speedEl = document.getElementById('speed')
 const text = "PaTshia Lee"
 let idx = 1;
-let speed = 100 / speedEl.value
+let speed = 300 / speedEl.value
 speedEl.addEventListener('input', (element) => {
     speed = 300 / element.target.value
 })
